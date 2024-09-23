457a08f3b18472e696381546da874bfb18b09116
kyun04 <efrencamposagrado0622@gmail.com>
2024-09-23 12:14:58 +0800

    stackable header

--- app/home_menu.tsx
+++ app/home_menu.tsx
@@ -1,4 +1,4 @@
-import { Text, View, TextInput, Image, ScrollView, FlatList, TouchableOpacity, Dimensions} from "react-native";
+import { Text, View, TextInput, Image, ScrollView, FlatList, TouchableOpacity, Dimensions, Animated} from "react-native";
 import React, {useState} from "react";
 import {useRouter} from "expo-router";
 
@@ -24,7 +24,15 @@ password:""
   }
 
 
+  const [scrollY] = useState(new Animated.Value(0));
+
   var windowX = Dimensions.get('window').width;
+
+  const headerHeight = scrollY.interpolate({
+    inputRange:[0,100],
+    outputRange: [165, 100],
+    extrapolate: 'clamp'  
+  })
 
   return (
 
@@ -34,22 +42,24 @@ password:""
       style={{
         backgroundColor: "#FFF",
         flex: 1,
-        padding: 20,
+        //padding: 20,
       }}
     >
 
-   
 
-    <View style = {{flexDirection:"row", alignItems: "center"}}>
+    <Animated.View style = {{flexDirection:"row", alignItems: "center", height: headerHeight}}>
       <TouchableOpacity onPress={onLogout}>
         <View style = {{marginTop: 45, marginLeft: 12, marginRight: 5}}><Image style = {{height: 55, width: 20}} source = {require('@/assets/images/arrowleft1.png')}></Image></View>
       </TouchableOpacity>
       <View>
         <Text style = {{marginTop: 50, marginLeft: 18, fontSize: 24, fontWeight:"bold"}}>Pages Admin</Text>
-        <Text style = {{marginTop: 2, marginLeft: 18, fontSize: 12}}>Manage the Whole Social Media like a Boss</Text>
+        
       </View>
-    </View>
+      </Animated.View>
  
+ <ScrollView scrollEventThrottle={16} onScroll = {Animated.event([{nativeEvent: {contentOffset: {y:scrollY}}}],{
+  useNativeDriver: false
+ })}>
    
    <View>
    <ScrollView horizontal style = {{marginTop: 25}} showsHorizontalScrollIndicator={false}>
@@ -78,13 +88,27 @@ password:""
      </FlatList>
    </View>
 
-   <View style={{width: windowX-50, height: 150, padding: 15,  marginLeft: 10, marginTop: 20, borderRadius: 10, backgroundColor:"#FFF", shadowColor:'#494547', shadowRadius: 35, shadowOpacity:0.1, shadowOffset: {width:-2, height:5},elevation: 10}}>
+   <View style={{width: windowX-22, height: 150, padding: 15,  marginLeft: 10, marginTop: 20, borderRadius: 10, backgroundColor:"#FFF", shadowColor:'#494547', shadowRadius: 35, shadowOpacity:0.1, shadowOffset: {width:-2, height:5},elevation: 10}}>
     <Text style = {{fontSize:16, fontWeight:"bold"}}>Monthly Usage</Text>
     <Text style = {{fontSize:12}}>People's average monthly usage</Text>
    </View>
       
+   <View style={{width: windowX-22, height: 150, padding: 15,  marginLeft: 10, marginTop: 20, borderRadius: 10, backgroundColor:"#FFF", shadowColor:'#494547', shadowRadius: 35, shadowOpacity:0.1, shadowOffset: {width:-2, height:5},elevation: 10}}>
+    <Text style = {{fontSize:16, fontWeight:"bold"}}>Monthly Usage</Text>
+    <Text style = {{fontSize:12}}>People's average monthly usage</Text>
+   </View>
 
+   <View style={{width: windowX-22, height: 150, padding: 15,  marginLeft: 10, marginTop: 20, borderRadius: 10, backgroundColor:"#FFF", shadowColor:'#494547', shadowRadius: 35, shadowOpacity:0.1, shadowOffset: {width:-2, height:5},elevation: 10}}>
+    <Text style = {{fontSize:16, fontWeight:"bold"}}>Monthly Usage</Text>
+    <Text style = {{fontSize:12}}>People's average monthly usage</Text>
+   </View>
 
+   <View style={{width: windowX-22, height: 150, padding: 15,  marginLeft: 10, marginTop: 20, marginBottom: 25, borderRadius: 10, backgroundColor:"#FFF", shadowColor:'#494547', shadowRadius: 35, shadowOpacity:0.1, shadowOffset: {width:-2, height:5},elevation: 10}}>
+    <Text style = {{fontSize:16, fontWeight:"bold"}}>Monthly Usage</Text>
+    <Text style = {{fontSize:12}}>People's average monthly usage</Text>
+   </View>
+
+   </ScrollView>
 
    </View>
 
